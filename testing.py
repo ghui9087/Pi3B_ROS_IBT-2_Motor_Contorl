d2a21ebe96f1bf8bedcd926656346d2dfd1bc711
ghui9087 <69405192+ghui9087@users.noreply.github.com>
2023-10-20 18:21:56 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -10,7 +10,7 @@ from std_msgs.msg import String, Float64
 # return 'HI' as the respond for the call
 def hello_callback(data):
     if data.data == "hello":
-        pub.publish("HI")
+        pub_hello.publish("HI")
 # Led Pin Testing funcation
 # THis is only workijng if you have connect the Led to the GPIO 1 And connect other end to GND
 def light_callback(data):
@@ -19,19 +19,35 @@ def light_callback(data):
         GPIO.setwarnings(False)
         GPIO.setup(1, GPIO.OUT)
         GPIO.output(1, GPIO.HIGH)
+        pub_light.publish("LED is On")
 
-def speed_callback(data):
-    speed = data.data
-    rospy.loginfo("Received speed: %s", speed)
+def motorContorl(data):
+    float motor1Speed = data.data[0]
+    float motor2Speed = data.data[1]
+    GPIO.setmode(GPIO.BCM)
+    # Motor 1
+    GPIO.setup(1,GPIO.OUT)
+    GPIO.setup(2,GPIO.OUT)
+    # Motor 2
+    GPIO.setup(3,GPIO.OUT)
+    GPIO.setup(4,GPIO.OUT)
+    pub_speed.publish("Speed: M1 = %s , M2 = %s", motor1Speed, motor2Speed)
+    if
+    
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("hello", String, hello_callback)
-    rospy.Subscriber("light", String, light_callback)
-    rospy.Subscriber("speed", Float64, speed_callback)
-    rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('test')
-    pub = rospy.Publisher('hello', String, queue_size=10)
-    listener()
+    rospy.init_node('listener')
+    pub_hello = rospy.Publisher('hello', String, latch=True, queue_size=10)
+    pub_light = rospy.Publisher('light', String, latch=True, queue_size=10)
+    pub_speed = rospy.Publisher('speed', Float64, latch=True, queue_size=10)
+    rospy.Subscriber("hello", String, hello_callback)
+    rospy.Subscriber("light", String, light_callback)
+    rospy.Subscriber("motorcontorl", Float64, motorContorl)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    finally:
+        GPIO.cleanup()
