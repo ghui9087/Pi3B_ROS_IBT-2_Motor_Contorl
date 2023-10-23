346bcc7f7b1cc9e9b4e427edac0e812a07dc32a4
ghui9087 <69405192+ghui9087@users.noreply.github.com>
2023-10-23 00:19:50 -0700

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# importaing the GPIO as the PBPI for 3B+ and 4 or higher
+import RPi.GPIO as GPIO
+import rospy
+import serial
+import time
+
+from std_msgs.msg import String, Float64
+
+# Testing funcation
+# return 'HI' as the respond for the call
+def hello_callback(data):
+    if data.data == "hello":
+        pub_hello.publish("HI")
+
+
+if __name__ == '__main__':
+    rospy.init_node('listener')
+    pub_hello = rospy.Publisher('hello', String, latch=True, queue_size=10)
+    rospy.Subscriber("hello", String, hello_callback)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    finally:
+        clearUP()
+        GPIO.cleanup()
